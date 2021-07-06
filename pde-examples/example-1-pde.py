da0b6ae044f0763b8d2eb40b3817a7dc82c7a456
gabriellemhobson <ghobson@ucsd.edu>
2021-07-06 18:17:19 -0400

    loading data into snapshots and controls is now working

--- pde-examples/example-1-pde.py
+++ pde-examples/example-1-pde.py
@@ -1,10 +1,23 @@
-import numpy as np 
+import numpy as np
 import pickle as pkl
+import pandas as pd
 
-fname = "$PWD/data/Advection_1D_k1.0_step0.pkl"
-file = open(fname, "rb")
-pde = pkl.load(file)
-file.close()
+summary = pd.read_csv('/Users/ghobson/Documents/Research/iPOD-SeisSol/pde-examples/data/Advection_1D_k1.0_step_summary.csv')
+print(summary.shape)
+file_names = summary.values[:,0]
+time_vals = summary.values[:,1]
 
-print(pde)
+snapshots = list()
+for fname in file_names:
+    print('fname',fname)
+    snap = np.fromfile(fname, dtype = np.float64) # correct dtype??
+    #df = pd.DataFrame(data)
+    snapshots.append(snap)
 
+# print(snapshots)
+
+controls = dict()
+controls['time'] = list()
+for t in time_vals:
+    controls['time'].append(t)
+print(controls)
