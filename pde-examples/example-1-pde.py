a9c2af4ac50f66114eff6a2239146e4a2baef928
gabriellemhobson <ghobson@ucsd.edu>
2021-07-19 13:32:40 -0700

    the function try_3() now calls the forward model to write the solutions, reads the solutions, constructs the pod basis, and evaluates it at an arbitrary point in time.

--- pde-examples/example-1-pde.py
+++ pde-examples/example-1-pde.py
@@ -126,7 +126,84 @@ def try_2():
     dset = grp.create_dataset('pod', data=x0)
     h5f.close()
 
+def try_3():
+    import sys
+    sys.path.append("/Users/ghobson/Documents/Research/pde_solver/togit/pde_solver/")
+    import pde_1D_advection as forward_model
+    import pdebase as pdebase
 
+    # run the forward model, this will solve the forward model and save every 1000 timesteps using pdebase
+    #forward_model.calling_advection_1d() 
+
+    # having some issues contacting pde.load_solution()
+    pde = pdebase.PDEBase('Advection_1D', ['k'], [1.0])
+    
+    # now to load the timesteps we saved
+    timesteps = np.linspace(0,20000,21,dtype=int)
+    snapshots = list()
+    for k in timesteps:
+        snap = pde.load_solution(n=k)
+        snap = np.array(snap)
+        snapshots.append(snap)
+    print(snapshots)
+
+    controls = dict()
+    controls['time'] = list()
+    for t in timesteps:
+        t = int(t)
+        controls['time'].append(t)
+    print(controls)
+
+    # Build the POD reduced order model
+    pod = podtools.PODMultivariate(remove_mean=False)
+    pod.database_append(controls, snapshots) # this can be called multiple times, but controls must always be the same
+    pod.setup_basis() # this is giving a runtime warning
+    pod.setup_interpolant(rbf_type='polyh', bounds_auto=True)
+
+    print('Singular values:\n', pod.singular_values)
+    e = pod.get_ric()
+    print('Relative Information Content (RIC):\n', e)
+
+    # LOOCV measures
+    measure = podtools.rbf_loocv(pod, norm_type="linf")
+    measure = np.absolute(measure)
+
+    ordering = np.argsort(measure)
+    print('m[smallest][Linf] =',('%1.4e' % measure[ordering[0]]))
+    print('m[largest ][Linf] =',('%1.4e' % measure[ordering[-1]]))
+    print('measure:\n', measure)
+    print('snapshot index min/max:', ordering[0], ordering[-1])
+
+    measure = podtools.rbf_loocv(pod, norm_type="rms")
+    measure = np.absolute(measure)
+
+    ordering = np.argsort(measure)
+    print('m[smallest][rms] =',('%1.4e' % measure[ordering[0]]))
+    print('m[largest ][rms] =',('%1.4e' % measure[ordering[-1]]))
+    print('measure:\n', measure)
+    print('snapshot index min/max:', ordering[0], ordering[-1])
+
+    # Evaluate the POD model at an arbitrary instant in time
+    x0 = pod.evaluate([2100.0])
+    #print(x0)
+
+    plt.cla()
+    plt.clf()
+    plt.close()
+    #plt.figure(figsize = (10,10))
+    plt.plot(np.linspace(0,1,len(x0)),x0)
+    plt.show()
+
+    '''
+    # Push POD data into a new H5 file
+    h5f = h5py.File("pod_eval.h5", "w")
+    grp = h5f.create_group("mesh0/")
+    #dset = h5f.create_dataset("mydataset", (100,), dtype='i')
+    dset = grp.create_dataset('pod', data=x0)
+    h5f.close()
+    '''
+    
 if __name__ == '__main__':
     # try_1()
-    try_2()
+    # try_2()
+    try_3()
